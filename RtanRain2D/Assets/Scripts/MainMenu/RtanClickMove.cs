626402e477aeb39bb1391e9d8e67efbfc0bb1b33
Park ha <115542242+parkha6@users.noreply.github.com>
2025-10-26 23:29:05 +0900

    feat 르탄이 걷기 애니메이션 & 방향전환 추가

--- RtanRain2D/Assets/Scripts/MainMenu/RtanClickMove.cs
+++ RtanRain2D/Assets/Scripts/MainMenu/RtanClickMove.cs
@@ -6,12 +6,20 @@ public class RtanClickMove : MonoBehaviour
     private float rtanSpeed = 1f;
     internal bool isMoving = false;
     internal Vector3 nextPosition;
+    private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidBody;
+    private Animator walkAnimator;
     private void Awake()
     {
+        walkAnimator = GetComponent<Animator>();
+        if (walkAnimator == null)
+        { Debug.Log("애니메이터가 없습니다."); }
         rigidBody = GetComponent<Rigidbody2D>();
         if (rigidBody == null)
         { Debug.Log("RigidBody 2d를 추가하지 않았습니다."); }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        { Debug.Log("스프라이트가 없습니다."); }
     }
     void Update()
     {
@@ -20,22 +28,32 @@ public class RtanClickMove : MonoBehaviour
             Vector3 screenPosition = Input.mousePosition;
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
             nextPosition = new Vector3(worldPosition.x, worldPosition.y, 0f);
+            if (nextPosition.x < transform.position.x)
+            { spriteRenderer.flipX = true; }
+            else if (nextPosition.x > transform.position.x)
+            { spriteRenderer.flipX = false; }
             isMoving = true;
         }
         if (isMoving)
-        { transform.position = Vector3.MoveTowards(transform.position, nextPosition, rtanSpeed * Time.deltaTime); }
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextPosition, rtanSpeed * Time.deltaTime);
+            walkAnimator.SetBool("isWalking", true);
+        }
         if (Mathf.Abs(transform.position.x - nextPosition.x) < 0.001f && Mathf.Abs(transform.position.y - nextPosition.y) < 0.001f)
         {
             nextPosition = transform.position;
+            walkAnimator.SetBool("isWalking", false);
             isMoving = false;
         }
     }
     private void OnCollisionStay2D(Collision2D other)
     {
+        /*
         if (other.gameObject.CompareTag("Ground"))
-        {
-            nextPosition = transform.position;
-            isMoving = false;
-        }
+        {*/
+        nextPosition = transform.position;
+        walkAnimator.SetBool("isWalking", false);
+        isMoving = false;
+        /*}*/
     }
 }
