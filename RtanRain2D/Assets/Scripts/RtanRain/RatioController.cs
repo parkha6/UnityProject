cba04091caefa54918a2ca6f7dd6e7d07e4affe8
Park ha <115542242+parkha6@users.noreply.github.com>
2025-10-26 13:58:46 +0900

    doc ReadMe용 스샷

---
+++ RtanRain2D/Assets/Scripts/RtanRain/RatioController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatioController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxRatio = 2.144f;
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Awake()
+    { rectTransform = GetComponent<RectTransform>(); }
+
+    // Update is called once per frame
+    void Start()
+    {
+        float currentRatio = (float)Screen.width / Screen.height;
+        if (currentRatio > maxRatio)
+        { SetAnchorToCenterBottom(); }
+        else if(currentRatio < maxRatio)
+        { SetAnchorToRightBottom(); }
+    }
+    private void SetAnchorToRightBottom()
+    {
+        rectTransform.anchoredPosition = new Vector3(-145f, 48f, 0f);
+        rectTransform.anchorMin = new Vector2(1f, 0f);
+        rectTransform.anchorMax = new Vector2(1f, 0f);
+        rectTransform.pivot = new Vector2(0.5f, 0.5f);
+    }
+
+    private void SetAnchorToCenterBottom()
+    {
+        rectTransform.anchoredPosition = new Vector3(1225f, 48f, 0f);
+        rectTransform.anchorMin = new Vector2(0.5f, 0f);
+        rectTransform.anchorMax = new Vector2(0.5f, 0f);
+        rectTransform.pivot = new Vector2(0.5f, 0.5f);
+    }
+}
